ec4f48ae28c26ad6a337353662331aa95575e842
Zakamaldin Andrey <zakamaldin.andrey@gmail.com>
2024-09-02 19:23:55 +0300

    Split job on two(branch+tags). Add registry id in Jenkins creds. Add installing some usefull jenkins plugins

--- diploma_project/outputs.tf
+++ diploma_project/outputs.tf
@@ -14,10 +14,6 @@ output "grafana_url" {
   value = "http://monitoring.zakamaldin.test/grafana/login"
 }
 
-output "registry_id" {
-  value = yandex_container_registry.zakamaldin-diploma-registry.id
-}
-
 output "setup_jenkins_command" {
   value = "ansible-playbook jenkins/setup_jenkins.yml -i jenkins/inventory/jenkins/hosts.yml"
 }
--- diploma_project/registry.tf
+++ diploma_project/registry.tf
@@ -20,8 +20,15 @@ resource "yandex_container_registry_iam_binding" "zakamaldin-diploma-registry-sa
   members     = ["serviceAccount:${yandex_iam_service_account.zakamaldin-diploma-registry-sa.id}"]
 }
 
+resource "null_resource" "registry_id" {
+  depends_on = [ yandex_container_registry.zakamaldin-diploma-registry ]
+  provisioner "local-exec" {
+    command = "echo '${yandex_container_registry.zakamaldin-diploma-registry.id}' > jenkins/registry_id"
+  }
+}
+
 resource "null_resource" "get_registry_token" {
-  depends_on = [yandex_container_registry.zakamaldin-diploma-registry, yandex_container_registry_iam_binding.zakamaldin-diploma-registry-sa-pusher ]
+  depends_on = [ yandex_container_registry.zakamaldin-diploma-registry, yandex_container_registry_iam_binding.zakamaldin-diploma-registry-sa-pusher ]
   provisioner "local-exec" {
     command = "/bin/bash get_registry_token.sh"
   }
